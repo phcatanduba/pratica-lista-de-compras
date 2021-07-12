b94a9af89fb0ee2e6a31a0ca24e46187f18fe9d6
Pedro Henrique <phcatanduba@gmail.com>
2021-07-12 14:46:37 -0300

    post and get methods added

--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -1,36 +1,36 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from 'react';
+import styled from 'styled-components';
 
 export default function InsertForm({ onAddItem }) {
-  const [text, setText] = useState("");
+    const [text, setText] = useState('');
 
-  function handleSubmit(e) {
-    e.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    const newItem = { text };
-    // Save item to server
+        const newItem = { text };
+        // Save item to server
+        const promise = axios.post('http://localhost:4000/item', text);
+        setText('');
+        onAddItem();
+    }
 
-    setText("");
-    onAddItem();
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-        placeholder="Insert a new item..."
-      />
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+                placeholder="Insert a new item..."
+            />
+        </form>
+    );
 }
 
 const Input = styled.input`
-  width: 400px;
-  line-height: 40px;
-  font-size: 20px;
-  border: none;
-  border-radius: 10px;
-  padding: 0 10px;
-  outline: none;
+    width: 400px;
+    line-height: 40px;
+    font-size: 20px;
+    border: none;
+    border-radius: 10px;
+    padding: 0 10px;
+    outline: none;
 `;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,41 +1,47 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import InsertForm from "./InsertForm";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import InsertForm from './InsertForm';
+import axios from 'axios';
 
 export default function ShoppingList() {
-  // Fake data
-  const [items, setItems] = useState([
-    { id: 1, text: "Pão" },
-    { id: 2, text: "Salsicha" },
-    { id: 3, text: "Ketchup" },
-  ]);
+    // Fake data
+    const [items, setItems] = useState([
+        { id: 1, text: 'Pão' },
+        { id: 2, text: 'Salsicha' },
+        { id: 3, text: 'Ketchup' },
+    ]);
 
-  useEffect(loadItems, []);
+    useEffect(loadItems, []);
 
-  function loadItems() {
-    // Get items from back-end and update state
-  }
+    function loadItems() {
+        // Get items from back-end and update state
+        const promise = axios.get('http://localhost:4000/items');
 
-  return (
-    <>
-      <InsertForm onAddItem={loadItems} />
-      <List>
-        {items.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </List>
-    </>
-  );
+        promise.then((res) => {
+            setItems(res.data);
+        });
+    }
+
+    return (
+        <>
+            <InsertForm onAddItem={loadItems} />
+            <List>
+                {items.map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </List>
+        </>
+    );
 }
 
 const List = styled.ul`
-  margin-top: 40px;
-  background: #fff;
-  width: 600px;
-  padding: 20px;
-  border-radius: 10px;
-  font-size: 25px;
-  padding-left: 50px;
-  line-height: 40px;
-  list-style-type: disc;
+    margin-top: 40px;
+    background: #fff;
+    width: 600px;
+    padding: 20px;
+    border-radius: 10px;
+    font-size: 25px;
+    padding-left: 50px;
+    line-height: 40px;
+    list-style-type: disc;
 `;
